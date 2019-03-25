0d46259034a49b356f013eaa3bfafe63ec529158
Owais Ahmed <oowais@gmail.com>
2019-03-25 23:11:11 +0100

    :shell: commit emojis

--- simple-notes-api/server.py
+++ simple-notes-api/server.py
@@ -1,8 +1,9 @@
+from json import dumps
+
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
-from flask_restful import Resource, Api
-from json import dumps
 from flask_jsonpify import jsonify
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
