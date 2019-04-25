091e2065f741576714dd1a085f02902e910c2b76
Owais Ahmed <oowais@gmail.com>
2019-04-25 22:13:58 +0200

    :expressionless: get, filter and create with slite
    wrap context if too long in notes

--- simple-notes-api/sqlite_db.py
+++ simple-notes-api/sqlite_db.py
@@ -6,50 +6,62 @@ class SqliteDb:
     def __init__(self):
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(__name__)
-        self.__conn = sqlite3.connect('db/notes.db')
-        self.__table_name = 'notes'
-        self.create_table()
+        self.__conn = sqlite3.connect('db/notes.db', check_same_thread=False)
+        self.create_tables()
 
-    def create_table(self):
-        c = self.__conn.cursor()
-        create_table = '''CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY,
-                        heading text, note_text text, last_edited text)'''
-        params = (self.__table_name,)
-        c.execute(create_table)
-        self.__conn.commit()
-        c.close()
+    def create_tables(self):
+        try:
+            c = self.__conn.cursor()
+            create_table = '''CREATE TABLE IF NOT EXISTS notes (id INTEGER PRIMARY KEY,
+                            heading text, note_text text, last_edited text)'''
+            c.execute(create_table)
+            self.__conn.commit()
+            c.close()
+        except sqlite3.Error as e:
+            self.logger.error("Database error: %s" % e)
 
     def get_all_notes(self):
         try:
-            records = []
-            # fetch all notes from table
-        except IndexError:
+            c = self.__conn.cursor()
+            get_notes = '''SELECT * FROM notes'''
+            c.execute(get_notes)
+            records = c.fetchall()
+
+            notes = []
+            for d in records:
+                notes.append({'id': d[0], 'heading': d[1], 'note_text': d[2], 'last_edited': d[3]})
+            return notes
+        except sqlite3.Error as e:
+            self.logger.error("Database error: %s" % e)
             return None
-        notes = []
-        for d in records:
-            notes.append({'id': d.get('id'), 'heading': d.get('heading'), 'note_text': d.get('note_text'),
-                          'last_edited': d.get('last_edited')})
-        return notes
 
     def get_filtered_notes(self, search_value):
         try:
-            records = []
-            # fetch all notes from table
-        except IndexError:
+            c = self.__conn.cursor()
+            filter_notes = '''SELECT * FROM notes WHERE heading LIKE ? OR note_text LIKE ?'''
+            params = ('%' + search_value + '%', '%' + search_value + '%')
+            c.execute(filter_notes, params)
+            records = c.fetchall()
+            notes = []
+            for d in records:
+                notes.append({'id': d[0], 'heading': d[1], 'note_text': d[2], 'last_edited': d[3]})
+            return notes
+        except sqlite3.Error as e:
+            self.logger.error("Database error: %s" % e)
             return None
-        notes = []
-        for d in records:
-            if search_value.lower() in d.get('heading').lower() or search_value.lower() in d.get('note_text').lower():
-                notes.append({'id': d.get('id'), 'heading': d.get('heading'), 'note_text': d.get('note_text'),
-                              'last_edited': d.get('last_edited')})
-        return notes
 
     def create_note(self, heading, note_text, last_edited):
-        # Fetch last id
-        # Then increment it to make curr_id
-        curr_id = 1
-        self.logger.info('Create note with id %d', curr_id)
-        # then insert in the table
+        try:
+            c = self.__conn.cursor()
+            create = '''INSERT INTO notes(heading,note_text,last_edited) VALUES (?,?,?)'''
+            params = (heading, note_text, last_edited)
+            c.execute(create, params)
+            self.__conn.commit()
+            c.close()
+            self.logger.info('Node Created')
+        except sqlite3.Error as e:
+            self.logger.error("Database error: %s" % e)
+            return None
 
     def update_note(self, id, heading, note_text, last_edited):
         # get the row with id and update other details
