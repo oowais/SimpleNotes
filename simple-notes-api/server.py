f4cd7d7d210266c3eb08c4b93cf5b16d19f384bd
Owais Ahmed <oowais@gmail.com>
2019-05-14 23:36:36 +0200

    :ice_cream: Formatting and requirements version added

--- simple-notes-api/server.py
+++ simple-notes-api/server.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import logging
 
 from flask import Flask, request
 from flask_cors import CORS
@@ -13,46 +14,59 @@ from sqlite_db import SqliteDb
 app = Flask(__name__)
 api = Api(app)
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 CORS(app)
 
-json_file = Path('db/Simple-Notes-DB.json')
+json_file = Path("db/Simple-Notes-DB.json")
 if json_file.is_file():
     db = SheetsDb()
+    logger.info("Using Google Sheets as DB")
 else:
     db = SqliteDb()
+    logger.info("Using local DB")
 
 
-@app.route("/notes", methods=['GET'])
+@app.route("/notes", methods=["GET"])
 def get_notes():
-    search = request.args.get('search', default=None, type=str)
+    search = request.args.get("search", default=None, type=str)
     if search is not None:
         return jsonify(db.get_filtered_notes(search))
     else:
         return jsonify(db.get_all_notes())
 
 
-@app.route('/notes/add', methods=['POST'])
+@app.route("/notes/add", methods=["POST"])
 def add_note():
     data = request.data.decode("utf-8")
     data_dict = json.loads(data)
     now = datetime.datetime.now()
     date = now.strftime("%d-%m-%Y %H:%M")
-    db.create_note(heading=data_dict.get("heading"), note_text=data_dict.get("text"), last_edited=date)
+    db.create_note(
+        heading=data_dict.get("heading"),
+        note_text=data_dict.get("text"),
+        last_edited=date,
+    )
     return jsonify(success=True)
 
 
-@app.route('/notes/edit', methods=['POST'])
+@app.route("/notes/edit", methods=["POST"])
 def edit_note():
     data = request.data.decode("utf-8")
     data_dict = json.loads(data)
     now = datetime.datetime.now()
     date = now.strftime("%d-%m-%Y %H:%M")
-    db.update_note(id=data_dict.get('id'), heading=data_dict.get('heading'), note_text=data_dict.get('text'),
-                   last_edited=date)
+    db.update_note(
+        id=data_dict.get("id"),
+        heading=data_dict.get("heading"),
+        note_text=data_dict.get("text"),
+        last_edited=date,
+    )
     return jsonify(success=True)
 
 
-@app.route('/notes/delete', methods=['POST'])
+@app.route("/notes/delete", methods=["POST"])
 def delete_note():
     note_id = request.data.decode("utf-8")
     result = db.delete_note(int(note_id))
@@ -62,7 +76,7 @@ def delete_note():
         return jsonify("Error in deleting!", success=False)
 
 
-@app.route('/feedback', methods=['POST'])
+@app.route("/feedback", methods=["POST"])
 def feedback():
     data = request.data.decode("utf-8")
     data_dict = json.loads(data)
@@ -74,7 +88,7 @@ def feedback():
     # return jsonify("some error", success=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     Check if simple notes db is present(meaning Google sheets db is setup)
     If not use sqlite
